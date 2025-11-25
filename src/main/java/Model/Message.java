1ca61629e46c03ece3074e4bfbdaa36cb2c27e12
Ramil <rrobsibe@gmail.com>
2025-11-25 12:31:18 +0300

    Написал класс Message

--- src/main/java/Model/Message.java
+++ src/main/java/Model/Message.java
@@ -1,4 +1,17 @@
 package Model;
 
 public class Message {
+    private String messageStroka;
+    private String date;
+    private boolean media;
+
+    public Message(String messageStroka,String date,boolean media) {
+        this.messageStroka = messageStroka;
+        this.date = date;
+        this.media = media;
+    }
+
+    public void setMessageStroka(String newStroka) {
+        this.messageStroka = newStroka;
+    }
 }
