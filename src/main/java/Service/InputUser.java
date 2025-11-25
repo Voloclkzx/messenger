58f562f96a231470d490b766647e75b7e4c15fb1
Voloclkzx <kartyshovvladimir@gmail.com>
2025-11-25 16:07:55 +0300

    Добавлен InputUser

--- src/main/java/Service/InputUser.java
+++ src/main/java/Service/InputUser.java
@@ -1,4 +1,32 @@
 package Service;
 
+import Model.User;
+
+import java.util.Scanner;
+
 public class InputUser {
+        public User checkUser() {
+            System.out.println("Введите логин, либо введите \"exit\", чтобы завершить программу: ");
+            Scanner scanner = new Scanner(System.in);
+            String login = scanner.nextLine();
+            System.out.println("Введите пароль, либо введите \"exit\", чтобы завершить программу: ");
+            String password = scanner.nextLine();
+            if (login.equals("exit") || password.equals("exit")) {
+                return null;
+            }
+            for (User user : User.getAll()) {
+                if (checkLoginPassword(login, password, user)) {
+                    return user;
+                }
+            }
+            return null;
+        }
+        boolean checkLoginPassword(String login, String password, User user) {
+            if (user.getUserName().equals(login) && user.getPassword().equals(password)) {
+                return true;
+            }
+            return false;
+        }
+
+
 }
