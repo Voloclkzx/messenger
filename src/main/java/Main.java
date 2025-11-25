b9fe6f220a0dba1c1914e711cceb5ddcd4d96a5e
Ramil <rrobsibe@gmail.com>
2025-11-25 16:34:00 +0300

    Вход пользователя

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import Model.User;
+import Service.InputUser;
 public class Main {
     public static void main(String[] args) {
+      InputUser inputUser = new InputUser();
+      User user = inputUser.checkUser();
 
+      if (user == null) {
+          System.out.println("22");
+      } else {
+          System.out.println("33");
+      }
     }
     public void createDatabase() {
 
